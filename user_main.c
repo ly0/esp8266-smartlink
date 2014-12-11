7f0647a8a4ce6a43a8272c63d1f1976813aad9e8
Yangjie Li(懒) <latyas@gmail.com>
2014-12-12 03:03:40 +0800

    Update user_main.c

--- user_main.c
+++ user_main.c
@@ -4,6 +4,14 @@
 #include "user_interface.h"
 #include "version.h"
 
+void smartlink_success(void *args)
+{
+	printf("smartlink_success");
+}
+void smartlink_failed(void *args)
+{
+	printf("smartlink_failed");
+}
 SLIST_HEAD(router_info_head, router_info) router_list;
 
 void ICACHE_FLASH_ATTR
@@ -16,6 +24,6 @@ void ICACHE_FLASH_ATTR
 user_init()
 {
     uart_init(115200, 115200);
-    smartlink_init(test, NULL);
+    smartlink_init(start_smartlink, NULL, smartlink_failed, 5);
 }
 
