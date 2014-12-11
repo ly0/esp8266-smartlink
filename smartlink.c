57e6651ccb15c0dad67884cf267c5937b9863453
Yangjie Li(懒) <latyas@gmail.com>
2014-12-12 03:01:49 +0800

    Update smartlink.c

--- smartlink.c
+++ smartlink.c
@@ -1,41 +1,67 @@
-#include "ets_sys.h"
+#include "c_types.h"
+#include "freertos/FreeRTOS.h"
 #include "osapi.h"
-#include "user_interface.h"
 #include "network_80211.h"
+#include "esp_sta.h"
 #define SMARTLK_TOKEN "<!-SL-!>"
-#define SMARTLK_TOKEN_LEN (os_strlen(SMARTLK_TOKEN))
+#define SMARTLK_TOKEN_LEN (strlen(SMARTLK_TOKEN))
 
-void smartlink_wifi_promiscuous_rx(uint8*, uint16);
+void smartlink_wifi_promiscuous_rx(uint8_t*, uint16);
 void smartlink_received(char*, char*);
+void delay_ms(uint16);
+void delay_s(uint16);
 
 void (*smartlink_received_cb)(void*) = NULL;
 void *smartlink_received_cb_args = NULL;
+void (*smartlink_failed_cb)() = NULL;
 
-void smartlink_init(void* cb, void* args)
-{	
-	os_printf("\n\n");
-	os_printf("[*] reset wifi config \n");
+uint8_t max_wait = 5;
+
+void delay_s(uint16 ss)
+{
+	uint16 i;
+	for(i = 0; i < ss; i++)
 	{
+		delay_ms(1000);
+	}	
+}
+void delay_ms(uint16 sms)
+{
+	uint16 i;
+	for(i = 0; i < sms; i++)
+	{
+		os_delay_us(1000);
+	}
+}
+void ICACHE_FLASH_ATTR
+smartlink_init(void* cb, void* args, void* fail, uint8_t max)
+{	
+	printf("\n\n");
+	printf("[*] reset wifi config \n");
+	{
+		wifi_station_disconnect();
 		wifi_station_set_config(NULL);
 	}
 
-	os_printf("[*] set opmode=0x01\n");
+	printf("[*] set opmode=0x01\n");
     wifi_set_opmode(0x01);
     
-    os_printf("[*] set smartlink_received callback function\n");
+    printf("[*] set smartlink_received callback function\n");
     smartlink_received_cb = cb;
     smartlink_received_cb_args = args;
+    smartlink_failed_cb = fail;
+    max_wait = max;
 
-    os_printf("[*] enable wifi promiscuous\n");
+    printf("[*] enable wifi promiscuous\n");
     wifi_promiscuous_enable(1);
     wifi_set_promiscuous_rx_cb(smartlink_wifi_promiscuous_rx);
 }
 
 void ICACHE_FLASH_ATTR
-smartlink_wifi_promiscuous_rx(uint8 *buf, uint16 len)
+smartlink_wifi_promiscuous_rx(uint8_t *buf, uint16 len)
 {
     uint16 i;
-    uint8 type;
+    uint8_t type;
 
     lpframectrl_80211 framectrl;
     struct router_info *info = NULL;
@@ -47,20 +73,20 @@ smartlink_wifi_promiscuous_rx(uint8 *buf, uint16 len)
     		ptagged_parameter tag = (ptagged_parameter)(buf + sizeof(probe_request));
     		if (tag->tag_length != 0)
     		{
-	    		uint8 ssid_buff[32];
-	    		os_memset(ssid_buff, 0, 32);
-	    		os_memcpy(ssid_buff, (uint8 *)tag + 2, tag->tag_length);
+	    		uint8_t ssid_buff[32];
+	    		memset(ssid_buff, 0, 32);
+	    		memcpy(ssid_buff, (uint8_t *)tag + 2, tag->tag_length);
 	    		char *pos = strstr(ssid_buff, SMARTLK_TOKEN);
 	    		if (pos != NULL)
 	    		{
-	    			uint8 ssid[40];
-	    			uint8 password[40];
-	    			os_memset(ssid, 0, 32);
-	    			os_memset(password, 0, 32);
+	    			uint8_t ssid[40];
+	    			uint8_t password[40];
+	    			memset(ssid, 0, 32);
+	    			memset(password, 0, 32);
 	    			/* ssid */
-	    			os_memcpy(ssid, ssid_buff, (uint8*)pos - ssid_buff);
+	    			memcpy(ssid, ssid_buff, (uint8_t*)pos - ssid_buff);
 	    			/* password */
-	    			os_memcpy(password, pos + SMARTLK_TOKEN_LEN, tag->tag_length - SMARTLK_TOKEN_LEN - os_strlen(ssid));
+	    			memcpy(password, pos + SMARTLK_TOKEN_LEN, tag->tag_length - SMARTLK_TOKEN_LEN - strlen(ssid));
 	    			smartlink_received(ssid, password);
 	    		}
     		}
@@ -70,13 +96,44 @@ smartlink_wifi_promiscuous_rx(uint8 *buf, uint16 len)
 void ICACHE_FLASH_ATTR
 smartlink_received(char* ssid, char* password)
 {
-	os_printf("[*] received SSID:%s PASSWORD:%s\n", ssid, password);
+	printf("[*] received SSID:%s PASSWORD:%s\n", ssid, password);
 	wifi_promiscuous_enable(0);
-	os_printf("[*] change mode: AP \n");
-	wifi_set_opmode(0x02);
-
-	if (smartlink_received_cb)
+	printf("[*] change mode: STATION \n");
+	wifi_set_opmode(0x01);
 	{
-		(*smartlink_received_cb)(smartlink_received_cb_args);
+		struct station_config config;
+		strcpy(config.ssid, ssid);
+		strcpy(config.password, password);
+		wifi_station_set_config(&config);
 	}
+	printf("[*] connecting to %s ... \n", ssid);
+	wifi_station_connect();
+
+	printf("[*] check connection to %s ... \n", ssid);
+	{
+		uint8_t status;
+		uint8_t retry;
+		for(retry = 0; retry < max_wait; ++retry)
+		{
+			status = wifi_station_get_connect_status();
+			if (STATION_IDLE == status || STATION_GOT_IP == status)
+			{	
+				printf("[*] connected to %s", ssid);
+				if (smartlink_received_cb)
+				{
+					(*smartlink_received_cb)(smartlink_received_cb_args);
+				}
+			} else {
+				printf("[*] probing AP:%s\n", ssid);
+				vTaskDelay(1000 / portTICK_RATE_MS);			
+			}
+		}	
+		
+	}
+
+	if (smartlink_failed_cb)
+	{
+		(*smartlink_failed_cb)();
+	}	
+
 }
